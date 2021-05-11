425821b2d3b275edd5da09f1d89921f884da4c5d
priya-002 <priya.md0002@gmail.com>
2021-05-11 22:35:58 +0530

    practice

--- test.js
+++ test.js
@@ -38,5 +38,52 @@ function circuitPower(voltage, current) {
 	return(voltage*current)
 	}
 	function howManySeconds(hours) {
-		return(hours*60*60)
+		console.log(hours*60*60)
 	}
+    howManySeconds(2);
+
+
+	function hello() {
+		return ("hello edabit.com")
+	}
+	function addition(a, b) {
+		return(a+b)
+	}
+	function triArea(base, height) {
+		return((base*height)/2)
+	}
+	function addition(a, b) {
+		return(a*b)
+	}
+	function squaed(b) {
+		return(b*b)
+	}
+	function calcAge(age) {
+		return(age*365)
+	}
+	function remainder(x, y) {
+		return(x%y)
+	}
+	function convert(minutes) {
+		return(minutes*60)
+	}
+	function getFirstValue(arr) {
+		return(arr[0])
+	}
+	function cubes(a) {
+		return (a**3)
+	}
+	function findPerimeter(length, width) {
+		return(2*(length+width))
+	}
+	function findPerimeter(length, width) {
+		return(2*(length+width))
+	}
+	function circuitPower(voltage, current) {
+		return(voltage*current)
+		}
+		function howManySeconds(hours) {
+			console.log(hours*60*60)
+		}
+		howManySeconds(2);
+	
